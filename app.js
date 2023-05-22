e3c383a5d14191d3c6914e10587f2de07e272f51
meligaunas <gaunasmelitza@gmail.com>
2023-05-22 13:27:58 -0300

    My first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const argv = require("process").argv;
+const sumar = require("./operaciones/sumar");
+const restar = require("./operaciones/restar");
+const multiplicar = require("./operaciones/multiplicar");
+const dividir = require("./operaciones/dividir");
+
+const operacion = argv[2];
+const numA = +argv[3];
+const numB = +argv[4];
+
+let resultado = 0;
+
+if (operacion === "sumar") {
+  resultado = sumar(numA, numB);
+
+} else if (operacion === "restar") {
+  resultado = restar(numA, numB);
+
+} else if (operacion === "multiplicar") {
+  resultado = multiplicar(numA, numB);
+
+} else if (operacion === "dividir") {
+  resultado = dividir(numA, numB);
+
+  if (numA === 0 && numB === 0) {
+    return console.log("0");
+  }
+  
+} else {
+    resultado = 'Operacion no reconocida';
+}
+
+console.log(resultado);
